f6eeb6b9ac39466fab79eb7100e829cf6cbf2a32
esu00756 <francho.garcia@es.abb.com>
2017-12-14 15:18:03 +0100

    first commit

---
+++ FizzBuzz.Library.Tests/FizzBuzzerTests.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz.Library.Tests
+{
+    [TestFixture]
+    public class FizzBuzzerTests
+    {
+         [Test]
+        public void BuzzerWhenDefaultReturnsInput(
+            [Values(1,2,4,7,8,11,13,14,16)] int input)
+         {
+            
+            string output = FizzBuzzer.getvalue(input);
+
+            Assert.AreEqual(input.ToString(), output);
+        }
+
+        [Test]
+        public void BuzzerWhenDiv3ReturnsFizz(
+            [Values(3,6,9,12)] int input)
+        {
+            string output = FizzBuzzer.getvalue(input);
+
+            Assert.AreEqual("Fizz", output);
+        }
+
+        [Test]
+        public void BuzzerWhenDiv5ReturnsBuzz(
+            [Values(5,10)] int input)
+        {
+            string output = FizzBuzzer.getvalue(input);
+
+            Assert.AreEqual("Buzz", output);
+        }
+
+        [Test]
+        public void BuzzerWhenDiv3and5ReturnsFizzBuzz(
+            [Values(15)] int input)
+        {
+            string output = FizzBuzzer.getvalue(input);
+
+            Assert.AreEqual("FizzBuzz", output);
+        }
+    }
+}
---
+++ FizzBuzz.Library/FizzBuzzer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz.Library
+{
+    public class FizzBuzzer
+    {
+        public static string getvalue(int input)
+        {
+            string output = string.Empty;            
+            string inputstr = input.ToString();
+            if (input % 3 == 0 || inputstr.Contains("3"))
+                output += "Fizz";
+            if (input % 5 == 0 || inputstr.Contains("5"))
+                output += "Buzz";
+            if (string.IsNullOrEmpty(output))
+                output = input.ToString();
+            return output;
+        }
+    }
+}
